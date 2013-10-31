dbd7a4d31b98cf2b3af7a32f6b98b87239020e61
DTrejo <david.trejo@dena.jp>
2013-10-31 16:57:30 -0700

    do npm install --save-dev, add --preview flag

--- index.js
+++ index.js
@@ -15,6 +15,7 @@ after running your package.json will look something like this
 
 var fs = require('fs')
 var path = require('path')
+var exec = require('child_process').exec
 
 var argv = require('optimist')
     .usage('Usage: $0 --code your/source/code [--package your/npm/module]')
@@ -43,27 +44,46 @@ var PKG_PATH = path.join(argv.package, 'package.json')
 
 var pkg = require(PKG_PATH);
 
-pkg.scripts = pkg.scripts || {}
-
-if (!argv.force) {
-  if (pkg.scripts.posttest) {
-    throw new Error(PKG_PATH + ' already contains a `posttest` property')
-  }
-  if (pkg.scripts.blanket) {
-    throw new Error(PKG_PATH + ' already contains a `banket` property')
-  }
+// put blanket in dev deps
+var install = 'npm install --save-dev blanket';
+if (!pkg.devDependencies.blanket && !argv.preview) {
+  console.log(install)
+  return exec(install, function (error, stdout, stderr) {
+      if (stdout) console.log(stdout)
+      if (stderr) console.log(stderr)
+      if (error !== null) return console.log('exec error: ' + error)
+      return keepGoing();
+  })
 }
+console.log('preview: ' + install)
+return keepGoing()
 
-var posttest = 'mocha --require blanket -R html-cov > coverage.html'
-    + ' && open coverage.html'
-pkg.scripts.posttest = posttest
 
-pkg.scripts.blanket = pkg.scripts.blanket || {}
+function keepGoing() {
+  // npm install might have changed it
+  pkg = JSON.parse(fs.readFileSync(PKG_PATH))
+  pkg.scripts = pkg.scripts || {}
 
-// TODO get blanket to support a "path" option.
-pkg.scripts.blanket.pattern = path.relative(path.dirname(argv.package), argv.code)
+  if (!argv.force) {
+    if (pkg.scripts.posttest) {
+      throw new Error(PKG_PATH + ' already contains a `posttest` property')
+    }
+    if (pkg.scripts.blanket) {
+      throw new Error(PKG_PATH + ' already contains a `banket` property')
+    }
+  }
 
-var str = JSON.stringify(pkg, null, 2)
-console.log(str)
-if (argv.preview) return
-fs.writeFileSync(PKG_PATH, str)
+  var posttest = 'mocha --require blanket -R html-cov > coverage.html'
+      + ' && open coverage.html'
+  pkg.scripts.posttest = posttest
+
+  pkg.scripts.blanket = pkg.scripts.blanket || {}
+
+  // TODO get blanket to support a "path" option.
+  pkg.scripts.blanket.pattern = path.relative(path.dirname(argv.package), argv.code)
+
+  var str = JSON.stringify(pkg, null, 2)
+  console.log(str)
+  if (argv.preview) return
+  fs.writeFileSync(PKG_PATH, str)
+}
